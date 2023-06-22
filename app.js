1d7fb7ca35196cc4b64d30c6c7b4a9826f83abf1
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-06-22 19:29:56 -0400

    ADD | Side Menu + Toggle Slide In Effect

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 class App {
   constructor() {
+    // variable
 
+    // dom elements
+    this.$sideNav = document.querySelector(".js-side-nav-container");
+    this.$menuBtn = document.querySelector(".js-menu-btn");
+
+    // functions here
+    this.handleEventListeners();
   }
 
-  handleEventListners() {
-    
+  handleEventListeners() {
+    document.addEventListener("click", (e) => {
+      // handle side nav toggle
+      this.toggleSideNav(e);
+    });
+    this.$sideNav.addEventListener("transitionend", function () {
+      this.classList.remove("side-nav--animatable");
+    });
+  }
+
+  toggleSideNav(e) {
+    if (e.target.closest(".js-menu-btn")) {
+      this.$sideNav.classList.add("side-nav--animatable", "side-nav--visible");
+    }
+
+    if (e.target.closest(".js-overlay") || e.target.matches(".js-close-btn")) {
+      this.$sideNav.classList.add("side-nav--animatable");
+      this.$sideNav.classList.remove("side-nav--visible");
+    }
   }
 }
 
