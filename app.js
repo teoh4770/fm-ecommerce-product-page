ba81a429c16d50d3caee008fac1799f5fd26a53e
Chee Kian Teoh <teoh4770@mylaurier.ca>
2023-06-23 18:04:59 -0400

    ADD | cart, thumbnails
    1. switching the image through thumbnails 2. toggle the cart

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 class App {
   constructor() {
     // variable
+    this.cartList = [];
 
     // dom elements
     this.$sideNav = document.querySelector(".js-side-nav-container");
     this.$menuBtn = document.querySelector(".js-menu-btn");
+    this.$headers = document.querySelectorAll(".header");
+
+    this.$largeImg = document.querySelector(
+      ".product-display-section__large-product-img"
+    );
+    this.$thumbnails = document.querySelectorAll(
+      ".product-display-section__thumbnails > img"
+    );
 
     // functions here
     this.handleEventListeners();
@@ -14,9 +23,25 @@ class App {
     document.addEventListener("click", (e) => {
       // handle side nav toggle
       this.toggleSideNav(e);
+      this.toggleCart(e);
     });
+
     this.$sideNav.addEventListener("transitionend", function () {
       this.classList.remove("side-nav--animatable");
+    });
+
+    this.$thumbnails.forEach((thumbnail) => {
+      thumbnail.addEventListener("click", (e) => {
+        const imgId = e.target.dataset.number;
+        const number = /\d/;
+
+        let srcValue = this.$largeImg.attributes.src.value;
+        let splitted = srcValue.split(number);
+
+        let newSrc = this.insertAndJoin(splitted, -1, imgId);
+
+        this.$largeImg.src = newSrc;
+      });
     });
   }
 
@@ -29,6 +54,30 @@ class App {
       this.$sideNav.classList.add("side-nav--animatable");
       this.$sideNav.classList.remove("side-nav--visible");
     }
+  }
+
+  toggleCart(e) {
+    const header = this.$headers[0];
+    const cartIsVisible = header.classList.contains("cart--visible");
+    const closeTargets =
+      !e.target.closest(".nav__cart-icon-container") &&
+      !e.target.closest(".cart");
+
+    if (e.target.closest(".nav__cart-icon-container") && !cartIsVisible) {
+      this.$headers.forEach((header) => {
+        header.classList.add("cart--visible");
+      });
+    }
+
+    if (cartIsVisible && closeTargets) {
+      this.$headers.forEach((header) => {
+        header.classList.remove("cart--visible");
+      });
+    }
+  }
+
+  insertAndJoin(array, index, newValue) {
+    return [...array.slice(0, index), newValue, ...array.slice(index)].join("");
   }
 }
 
